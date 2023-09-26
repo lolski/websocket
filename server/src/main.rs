a0037c0628601f5dd0e5ae5b637433961b2566a6
Ganeshwara Hananda <ganesh@vaticle.com>
2023-09-26 13:45:48 +0100

    /websocket - ping pong

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,6 +2,7 @@ use std::net::{IpAddr, SocketAddr};
 use axum::Router;
 use axum::extract::ws::{Message, WebSocketUpgrade};
 use axum::extract::ws::WebSocket;
+use axum::response::Response;
 use axum::routing::get;
 use futures::{SinkExt, StreamExt};
 
@@ -20,24 +21,38 @@ async fn about_handler<'l1>() -> &'l1 str {
     "return value from 'about_handler'"
 }
 
-async fn websocket_handler_ws_http_upgrade(ws_http_upgrade: WebSocketUpgrade) -> () {
+async fn websocket_handler_ws_http_upgrade(ws_http_upgrade: WebSocketUpgrade) -> Response {
     println!("websocket_handler_ws_http_upgrade: triggered");
-    ws_http_upgrade.on_upgrade(websocket_handler);
+    return ws_http_upgrade.on_upgrade(websocket_handler);
 }
 
 async fn websocket_handler(ws: WebSocket) -> () {
     println!("websocket_handler: triggered");
     let (mut ws_sender, mut ws_receiver) = ws.split();
-    while let Some(try_msg) = ws_receiver.next().await {
-        match try_msg {
-            Ok(msg_obj) => {
+    while let Some(try_request) = ws_receiver.next().await {
+        match try_request {
+            Ok(msg_request) => {
                 println!("websocket_handler: new message received");
-                let msg_value = msg_obj.to_text().unwrap();
-                ws_sender.send(Message::Text(msg_value.to_string())).await.unwrap();
+                let request_value = msg_request.to_text().unwrap();
+                let response = Message::Text(format!("response for '{}'", request_value.to_string()));
+                let try_send = ws_sender.send(response).await;
+                match try_send {
+                    Ok(()) => {}
+                    Err(e) => {
+                        break;
+                    }
+                }
             }
             Err(e) => {
                 println!("websocket_handler: an error occurred: {}", e);
-                ws_sender.send(Message::Text(e.to_string())).await.unwrap();
+                let response = Message::Text(format!("response for the following error '{}'", e.to_string()));
+                let try_send = ws_sender.send(response).await;
+                match try_send {
+                    Ok(()) => {}
+                    Err(e) => {
+                        break;
+                    }
+                }
             }
         }
     }

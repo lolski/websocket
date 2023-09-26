84f2c5e181d673e2532c2e4e888a3c30f02a3a73
Ganeshwara Hananda <ganesh@vaticle.com>
2023-09-26 13:20:11 +0100

    /websocket - wip

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,12 +1,15 @@
 use std::net::{IpAddr, SocketAddr};
-use axum::{Router, Server};
+use axum::Router;
+use axum::extract::ws::WebSocketUpgrade;
+use axum::extract::ws::WebSocket;
 use axum::routing::get;
+use futures::StreamExt;
 
 #[tokio::main]
 async fn main() -> () {
     let router = Router::new()
         .route("/about", get(about_handler))
-        .route("/websocket", get(websocket_handler));
+        .route("/websocket", get(websocket_handler_ws_http_upgrade));
     let addr = addr();
     let axum_builder = axum::Server::bind(&addr);
     println!("server started");
@@ -17,8 +20,18 @@ async fn about_handler<'l1>() -> &'l1 str {
     "return value from 'about_handler'"
 }
 
-async fn websocket_handler() -> () {
+async fn websocket_handler_ws_http_upgrade(ws_http_upgrade: WebSocketUpgrade) -> () {
+    ws_http_upgrade.on_upgrade(websocket_handler);
+}
 
+async fn websocket_handler(ws: WebSocket) -> () {
+    let (ws_sender, mut ws_receiver) = ws.split();
+    while let Some(try_msg) = ws_receiver.next().await {
+        match try_msg {
+            Ok(msg) => {}
+            Err(e) => {}
+        }
+    }
 }
 
 fn addr() -> SocketAddr {

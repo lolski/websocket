d67231e81b3e484f3403b1a28be7fde0f6a8a680
Ganeshwara Hananda <ganesh@vaticle.com>
2023-09-26 12:49:31 +0100

    Added /about and /websocket endpoint

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,10 +1,30 @@
-use axum::{
-    routing::get,
-    Router,
-};
+use std::net::{IpAddr, SocketAddr};
+use axum::{Router, Server};
+use axum::routing::get;
 
 #[tokio::main]
-async fn main() -> Result<(), ()> {
-    println!("Hello, world!");
-    return Ok(());
+async fn main() -> () {
+    let router = Router::new()
+        .route("/about", get(about_handler))
+        .route("/websocket", get(websocket_handler));
+    let addr = addr();
+    let axum_builder = axum::Server::bind(&addr);
+    println!("server started");
+    axum_builder.serve(router.into_make_service()).await.unwrap();
+}
+
+async fn about_handler<'l1>() -> &'l1 str {
+    "return value from 'about_handler'"
+}
+
+async fn websocket_handler() -> () {
+
+}
+
+fn addr() -> SocketAddr {
+    let ip_array: [u8; 4] = [127, 0, 0, 1];
+    let ip: IpAddr = IpAddr::from(ip_array);
+    let port: u16 = 10000;
+    let addr: SocketAddr = SocketAddr::new(ip, port);
+    addr
 }

1384ce605b8341dce8b4b3445daed1ef6b2c8acc
Ganeshwara Hananda <ganesh@vaticle.com>
2023-10-04 16:24:26 +0100

    implement connection - wip

--- client/src/app/service/connection.service.ts
+++ client/src/app/service/connection.service.ts
@@ -15,36 +15,46 @@ export class ConnectionService {
   }
 
   private open(): WebSocket {
-    console.log("ConnectionService - open started")
-
     let ws = new WebSocket(this.url)
+    console.log("ConnectionService - open started. readyState = {}", this.readyState(ws))
     ws.onopen = () => this.onOpen(this)
     ws.onerror = (error: any) => this.onError(this, error)
     ws.onclose = (closeEvt: CloseEvent) => this.onClose(this, closeEvt)
-    console.log("ConnectionService - open ended")
+    console.log("ConnectionService - open ended. readyState = {}", this.readyState(ws))
     return ws
   }
 
-  private close(ws: WebSocket): void {
-    this.websocket.close()
-  }
-
   private onOpen(connSvc: ConnectionService): void {
-    console.log("ConnectionService - opened {}", connSvc)
+    console.log("ConnectionService - opened. readyState = {}", this.readyState(connSvc.websocket))
   }
 
   private onError(connSvc: ConnectionService, error: any): void {
-    console.log("ConnectionService - errored")
+    console.log("ConnectionService - errored. readyState = {}", this.readyState(connSvc.websocket))
   }
 
   private onClose(connSvc: ConnectionService, closeEvt: CloseEvent): void {
-    console.log("ConnectionService - closed")
-    this.close(this.websocket)
+    console.log("ConnectionService - closed. readyState = {}, closeEvt = {}", this.readyState(this.websocket), closeEvt)
+    if (closeEvt.wasClean) throw new Error("unexpected")
     setTimeout(() => {
           console.log("timeout, {}", connSvc)
           connSvc.websocket = connSvc.open()
         },
         this.reopenDelayMs
     )
+  }
+
+  private readyState(ws: WebSocket): string {
+    switch (ws.readyState) {
+      case WebSocket.CONNECTING:
+        return "CONNECTING";
+      case WebSocket.OPEN:
+        return "OPEN";
+      case WebSocket.CLOSING:
+        return "CLOSING";
+      case WebSocket.CLOSED:
+        return "CLOSED";
+      default:
+        throw new Error("The value of readyState is invalid")
+    }
   }
 }

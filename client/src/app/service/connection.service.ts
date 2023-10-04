67e9b3b0101feca3cc7546920cfa3ad74a167766
Ganeshwara Hananda <ganesh@vaticle.com>
2023-10-04 18:20:46 +0100

    resillient websocket

--- client/src/app/service/connection.service.ts
+++ client/src/app/service/connection.service.ts
@@ -4,40 +4,49 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ConnectionService {
-
   private readonly url: string = "ws://localhost:8080/connection"
+  private readonly heartbeatIntervalMs: number = 2000
   private readonly reopenDelayMs: number = 2000
 
-  private websocket: WebSocket
+  websocket: WebSocket
+  private heartbeat: Heartbeat
 
   constructor() {
-    this.websocket = this.open()
+    this.websocket = this.ws()
+    this.heartbeat = new Heartbeat(this.websocket, this.heartbeatIntervalMs)
   }
 
-  private open(): WebSocket {
+  private ws(): WebSocket {
     let ws = new WebSocket(this.url)
     console.log("ConnectionService - open started. readyState = {}", this.readyState(ws))
-    ws.onopen = () => this.onOpen(this)
-    ws.onerror = (error: any) => this.onError(this, error)
-    ws.onclose = (closeEvt: CloseEvent) => this.onClose(this, closeEvt)
+    ws.onopen = () => this.wsOpened(this)
+    ws.onerror = (error: any) => this.wsErrored(this, error)
+    ws.onclose = (closeEvt: CloseEvent) => this.wsClosed(this, closeEvt)
     console.log("ConnectionService - open ended. readyState = {}", this.readyState(ws))
     return ws
   }
 
-  private onOpen(connSvc: ConnectionService): void {
+  private wsOpened(connSvc: ConnectionService): void {
     console.log("ConnectionService - opened. readyState = {}", this.readyState(connSvc.websocket))
+    if (this.heartbeat.isActive()) throw new Error("assertion error")
+    this.heartbeat.activate()
   }
 
-  private onError(connSvc: ConnectionService, error: any): void {
+  private wsErrored(connSvc: ConnectionService, error: any): void {
     console.log("ConnectionService - errored. readyState = {}", this.readyState(connSvc.websocket))
   }
 
-  private onClose(connSvc: ConnectionService, closeEvt: CloseEvent): void {
-    console.log("ConnectionService - closed. readyState = {}, closeEvt = {}", this.readyState(this.websocket), closeEvt)
-    if (closeEvt.wasClean) throw new Error("unexpected")
+  private wsClosed(connSvc: ConnectionService, closeEvt: CloseEvent): void {
+    console.log("ConnectionService - closed. readyState = {}", this.readyState(connSvc.websocket))
+    if (closeEvt.wasClean) throw new Error("assertion error")
+    if (this.heartbeat.isActive()) this.heartbeat.deactivate()
+    this.scheduleReopen(connSvc)
+  }
+
+  private scheduleReopen(connSvc: ConnectionService) {
     setTimeout(() => {
           console.log("timeout, {}", connSvc)
-          connSvc.websocket = connSvc.open()
+          connSvc.websocket = connSvc.ws()
         },
         this.reopenDelayMs
     )
@@ -54,7 +63,45 @@ export class ConnectionService {
       case WebSocket.CLOSED:
         return "CLOSED";
       default:
-        throw new Error("The value of readyState is invalid")
+        throw new Error("assertion error")
     }
+  }
+
+  public send(data: string) {
+    this.websocket.send(data)
+  }
+}
+
+class Heartbeat {
+  private readonly intervalMs: number
+  private intervalId: number | undefined
+  private readonly websocket: WebSocket
+
+  constructor(ws: WebSocket, intervalMs: number) {
+    this.websocket = ws
+    this.intervalMs = intervalMs
+  }
+
+  isActive() {
+    return this.intervalId !== undefined;
+  }
+
+  activate() {
+    if (this.intervalId !== undefined) throw new Error("assertion error")
+
+    this.intervalId = setInterval(
+        () => {
+          console.log("ping")
+          this.websocket.send("ping")
+        },
+        this.intervalMs
+    )
+  }
+
+  deactivate() {
+    if (this.intervalId === undefined) throw new Error("assertion error")
+
+    clearInterval(this.intervalId)
+    this.intervalId = undefined
   }
 }

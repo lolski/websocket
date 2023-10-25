9f9651965b69b42912f4f6075ad135663d3ea85c
Ganeshwara Hananda <ganesh@vaticle.com>
2023-10-25 13:11:52 +0100

    Add login and chatroom components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatroomComponent } from './page/chatroom/chatroom.component';
+import { LoginComponent } from './page/login/login.component';
 import { ServiceModule } from "./service/service.module";
 import { WidgetModule } from "./widget/widget.module";
 
 @NgModule({
   declarations: [
       AppComponent,
+      ChatroomComponent,
+      LoginComponent,
   ],
   imports: [
       AppRoutingModule,

10e903a90d6d64b9a2ab38762cfe60002f0e7820
Ganeshwara Hananda <ganesh@vaticle.com>
2023-10-27 15:12:27 +0100

    update router

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,11 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { ChatroomComponent } from "./page/chatroom/chatroom.component";
 import { LoginComponent } from "./page/login/login.component";
-import {SettingComponent} from "./page/setting/setting.component";
+import { SettingComponent } from "./page/setting/setting.component";
 
 const routes: Routes = [
-  { path: '', component: ChatroomComponent },
   { path: 'login', component: LoginComponent },
+  { path: '', component: ChatroomComponent },
   { path: 'setting', component: SettingComponent },
 ];
 
